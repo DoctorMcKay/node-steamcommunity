666226731e68b94b952b8b94a9332b4f7f7b1461
Arkadiusz Sygulski <arkadiusz@sygulski.pl>
2016-10-27 22:18:31 +0200

    Remove unused function: handleNewConfirmation

--- components/confirmations.js
+++ components/confirmations.js
@@ -366,28 +366,6 @@ SteamCommunity.prototype.checkConfirmations = function() {
 			self._confirmationTimer = setTimeout(self.checkConfirmations.bind(self), self._confirmationPollInterval);
 		}
 	}
-
-	function handleNewConfirmation(conf, handleNumber) {
-		self._knownConfirmations[conf.id] = conf;
-
-		// Delay them by 1 second per new confirmation that we see, so that keys won't be the same.
-		setTimeout(function() {
-			if(self._identitySecret) {
-				self.emit('debug', 'Accepting confirmation #' + conf.id);
-				var time = Math.floor(Date.now() / 1000);
-				conf.respond(time, SteamTotp.getConfirmationKey(self._identitySecret, time, "allow"), true, function(err) {
-					if (err) {
-						self.emit('debug', "Can't accept confirmation #" + conf.id + ": " + err.message);
-					}
-
-					// We'll just retry next time we poll
-					delete self._knownConfirmations[conf.id];
-				});
-			} else {
-				self.emit('newConfirmation', conf);
-			}
-		}, handleNumber * 1000);
-	}
 };
 
 SteamCommunity.prototype._confirmationCheckerGetKey = function(tag, callback) {
