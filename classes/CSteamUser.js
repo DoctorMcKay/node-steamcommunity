868ce9cdb4b8f2a88acac7c06545ffc9a7cf8504
Revadike <revadike@outlook.com>
2019-04-05 00:10:00 +0200

    Added missing functions

--- classes/CSteamUser.js
+++ classes/CSteamUser.js
@@ -148,6 +148,14 @@ CSteamUser.prototype.comment = function(message, callback) {
 	this._community.postUserComment(this.steamID, message, callback);
 };
 
+CSteamUser.prototype.deleteComment = function(commentID, callback) {
+	this._community.deleteUserComment(this.steamID, commentID, callback);
+};
+
+CSteamUser.prototype.getUserComments = function(options, callback) {
+	this._community.postUserComment(this.steamID, options, callback);
+};
+
 CSteamUser.prototype.inviteToGroup = function(groupID, callback) {
 	this._community.inviteUserToGroup(this.steamID, groupID, callback);
 };
