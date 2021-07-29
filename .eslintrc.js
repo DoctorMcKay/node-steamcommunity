e252efc455f5140495b0b590fc3c8096f8c3bdb5
Alex Corn <mckay@doctormckay.com>
2021-07-29 03:15:38 -0400

    Change all eslint rules to errors so they'll hit on CI

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,17 +12,17 @@ module.exports = {
 		// Use tabs for indentation and require 'case' in switch to be indented 1 level (default 0)
 		indent: ['error', 'tab', {SwitchCase: 1}],
 		// Single quotes for strings
-		quotes: ['warn', 'single'],
+		quotes: ['error', 'single'],
 		// Always require semicolons
 		semi: ['error', 'always'],
 		// Don't use 'var'
-		'no-var': 'warn',
+		'no-var': 'error',
 		// Only use quotes in object literal keys as needed
-		'quote-props': ['warn', 'as-needed'],
+		'quote-props': ['error', 'as-needed'],
 		// Don't allow trailing spaces after a line
-		'no-trailing-spaces': 'warn',
+		'no-trailing-spaces': 'error',
 		// Require spaces before and after keywords (like "if")
-		'keyword-spacing': 'warn',
+		'keyword-spacing': 'error',
 		// Don't allow unused variables, but allow unused function args (e.g. in callbacks) and global vars
 		'no-unused-vars': ['error', {vars: 'local', args: 'none'}]
 	}
