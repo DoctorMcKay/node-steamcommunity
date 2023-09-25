2d76dcea6c7d78229eedd95e72031c654e373007
3urobeat <35304405+3urobeat@users.noreply.github.com>
2023-09-25 22:53:39 +0200

    Add support for following & unfollowing curators

--- components/groups.js
+++ components/groups.js
@@ -730,3 +730,67 @@ SteamCommunity.prototype.respondToAllGroupJoinRequests = function(gid, approve, 
 		}
 	}, "steamcommunity");
 };
+
+/**
+ * Follows a curator page
+ * @param {string} clanid - ID of the curator
+ * @param {function} callback - Takes only an Error object/null as the first argument
+ */
+SteamCommunity.prototype.followCurator = function(clanid, callback) {
+	this.httpRequestPost({
+		"uri": "https://store.steampowered.com/curators/ajaxfollow",
+		"form": {
+			"clanid": clanid,
+			"sessionid": this.getSessionID(),
+			"follow": 1
+		},
+		"json": true
+	}, (err, res, body) => {
+		if (!callback) {
+			return;
+		}
+
+		if (err) {
+			callback(err);
+			return;
+		}
+
+		if (body.success) {
+			callback(null);
+		} else {
+			callback(new Error(EResult[body] || ("Error " + body)));
+		}
+	}, "steamcommunity");
+};
+
+/**
+ * Unfollows a curator page
+ * @param {string} clanid - ID of the curator
+ * @param {function} callback - Takes only an Error object/null as the first argument
+ */
+SteamCommunity.prototype.unfollowCurator = function(clanid, callback) {
+	this.httpRequestPost({
+		"uri": "https://store.steampowered.com/curators/ajaxfollow",
+		"form": {
+			"clanid": clanid,
+			"sessionid": this.getSessionID(),
+			"follow": 0
+		},
+		"json": true
+	}, (err, res, body) => {
+		if (!callback) {
+			return;
+		}
+
+		if (err) {
+			callback(err);
+			return;
+		}
+
+		if (body.success) {
+			callback(null);
+		} else {
+			callback(new Error(EResult[body] || ("Error " + body)));
+		}
+	}, "steamcommunity");
+};
