b654a3435073b7885dec058447ebf3b5ac21e4c7
3urobeat <35304405+3urobeat@users.noreply.github.com>
2024-02-15 21:45:35 +0100

    feat(Reviews): Init

---
+++ classes/CSteamReviews.js
@@ -0,0 +1,16 @@
+const Cheerio = require('cheerio');
+const SteamID = require('steamid');
+const StdLib = require('@doctormckay/stdlib');
+
+const SteamCommunity  = require('../index.js');
+const Helpers = require('../components/helpers.js');
+
+
+SteamCommunity.prototype.getSteamReview = function(userID, appID, callback) {
+
+};
+
+
+function CSteamReview(community, data) {
+
+};
